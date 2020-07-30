d73aff32fd5390e2a91910626b2a86994bdbcfca
Phuong Le <phuonganh.le0312@gmail.com>
2020-07-29 22:19:21 -0400

    added initial questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const { start } = require("repl");
 const connection =
 mysql.createConnection({
     host: "localhost",
@@ -12,3 +13,32 @@ mysql.createConnection({
     database: "employee"
 
 });
+//connect to mysql server and database
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("You are connected");
+    //run function
+    questions();
+});
+
+function questions() {
+    inquirer.prompt({
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+          "Browse Company",
+          "Add Entry To Company",
+          "Update Company",
+      ],
+    })
+    .then((choice) => {
+        if (choice.action === "Browse Company") {
+            browseCompany();
+        } else if (choice.action === "Add Entry To Company") {
+            addEntry();
+        } else if (choice.action === "Update Company") {
+            updateCompany();
+        }
+    });
+}
